94d6dc157efa766bcde33a1bfe9cea35642107e7
nabarinoou <43375465+nabarinoou@users.noreply.github.com>
2018-09-20 14:46:40 +0300

    Add files via upload

---
+++ mf/js/main.js
@@ -0,0 +1,101 @@
+(function(){
+    // Alternate Fixed & Scroll Backgrounds - by CodyHouse.co
+	// toggle main navigation on mobile
+	var mainNav = document.getElementsByClassName('js-main-nav')[0];
+	if(mainNav) {
+		mainNav.addEventListener('click', function(event){
+			if( hasClass(event.target, 'js-main-nav') ){
+				var navList = mainNav.getElementsByTagName('ul')[0];
+				toggleClass(navList, 'cd-main-nav__list--is-visible', !hasClass(navList, 'cd-main-nav__list--is-visible'));
+			} 
+		});
+	}
+	
+	//class manipulations - needed if classList is not supported
+	function hasClass(el, className) {
+	  	if (el.classList) return el.classList.contains(className);
+	  	else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+	}
+	function addClass(el, className) {
+		var classList = className.split(' ');
+	 	if (el.classList) el.classList.add(classList[0]);
+	 	else if (!hasClass(el, classList[0])) el.className += " " + classList[0];
+	 	if (classList.length > 1) addClass(el, classList.slice(1).join(' '));
+	}
+	function removeClass(el, className) {
+		var classList = className.split(' ');
+	  	if (el.classList) el.classList.remove(classList[0]);	
+	  	else if(hasClass(el, classList[0])) {
+	  		var reg = new RegExp('(\\s|^)' + classList[0] + '(\\s|$)');
+	  		el.className=el.className.replace(reg, ' ');
+	  	}
+	  	if (classList.length > 1) removeClass(el, classList.slice(1).join(' '));
+	}
+	function toggleClass(el, className, bool) {
+		if(bool) addClass(el, className);
+		else removeClass(el, className);
+	}
+})();
+	//Выпадающее меню.
+jQuery(document).ready(function($){
+	var $lateral_menu_trigger = $('#cd-menu-trigger'),
+		$content_wrapper = $('.cd-main-content'),
+		$navigation = $('header');
+
+	//open-close lateral menu clicking on the menu icon
+	$lateral_menu_trigger.on('click', function(event){
+		event.preventDefault();
+		
+		$lateral_menu_trigger.toggleClass('is-clicked');
+		$navigation.toggleClass('lateral-menu-is-open');
+		$content_wrapper.toggleClass('lateral-menu-is-open').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+			// firefox transitions break when parent overflow is changed, so we need to wait for the end of the trasition to give the body an overflow hidden
+			$('body').toggleClass('overflow-hidden');
+		});
+		$('#cd-lateral-nav').toggleClass('lateral-menu-is-open');
+		
+		//check if transitions are not supported - i.e. in IE9
+		if($('html').hasClass('no-csstransitions')) {
+			$('body').toggleClass('overflow-hidden');
+		}
+	});
+
+	//close lateral menu clicking outside the menu itself
+	$content_wrapper.on('click', function(event){
+		if( !$(event.target).is('#cd-menu-trigger, #cd-menu-trigger span') ) {
+			$lateral_menu_trigger.removeClass('is-clicked');
+			$navigation.removeClass('lateral-menu-is-open');
+			$content_wrapper.removeClass('lateral-menu-is-open').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+				$('body').removeClass('overflow-hidden');
+			});
+			$('#cd-lateral-nav').removeClass('lateral-menu-is-open');
+			//check if transitions are not supported
+			if($('html').hasClass('no-csstransitions')) {
+				$('body').removeClass('overflow-hidden');
+			}
+
+		}
+	});
+
+	//open (or close) submenu items in the lateral menu. Close all the other open submenu items.
+	$('.item-has-children').children('a').on('click', function(event){
+		event.preventDefault();
+		$(this).toggleClass('submenu-open').next('.sub-menu').slideToggle(200).end().parent('.item-has-children').siblings('.item-has-children').children('a').removeClass('submenu-open').next('.sub-menu').slideUp(200);
+	});
+});
+//флип картинок
+(function() {
+	var cards = document.querySelectorAll(".card.effect__click");
+	for ( var i  = 0, len = cards.length; i < len; i++ ) {
+	  var card = cards[i];
+	  clickListener( card );
+	}
+  
+	function clickListener(card) {
+	  card.addEventListener( "click", function() {
+		var c = this.classList;
+		c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
+	  });
+	}
+  })();
+  
